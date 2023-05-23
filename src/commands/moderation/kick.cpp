ed7cbccb8547ef136cf463db401260b9aa985e37
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-05-24 01:47:04 +0700

    [Update]: Bug fixed

--- src/commands/moderation/kick.cpp
+++ src/commands/moderation/kick.cpp
@@ -9,31 +9,13 @@ void kick(dpp::cluster& client, const dpp::slashcommand_t& event)
 	const auto guild_id = event.command.guild_id;
 
 	const auto guild = dpp::find_guild(guild_id);
-	const auto is_error = error_check(client, event, user_id, dpp::p_kick_members, "kick");
-
-	std::string description;
-	dpp::user user = client.user_get_sync(user_id);
-
 	const auto guild_member = guild->members.find(user_id);
-	const auto client_roles = dpp::find_guild_member(guild_id, client.me.id).roles;
-	const auto user_roles = dpp::find_guild_member(guild_id, user_id).roles;
-
-	uint8_t client_pos;
-	uint8_t	user_pos;
 
 	bool is_higher = false;
 	bool is_failed = false;
 
-	if (!client_roles.empty() && !user_roles.empty() && (guild_member != guild->members.end()))
-	{
-		const auto client_pos = dpp::find_role(client_roles.at(0))->position;
-		const auto user_pos = dpp::find_role(user_roles.at(0))->position;
-
-		if (client_pos < user_pos) {
-			description = fmt::format("Cannot kick `{}` because my role is lower", user.username);
-			is_higher = true;
-		}
-	}
+	std::string description;
+	dpp::user user = client.user_get_sync(user_id);
 
 	if (guild_member == guild->members.end())
 	{
@@ -43,24 +25,45 @@ void kick(dpp::cluster& client, const dpp::slashcommand_t& event)
 		is_failed = true;
 	}
 
-	if (!std::get<bool>(is_error) && !is_higher && !is_failed)
+	if (!is_failed)
 	{
-		const auto reason_param = event.get_parameter("reason");
-		const auto reason = std::holds_alternative<std::string>(reason_param)
-			? std::get<std::string>(reason_param)
-			: "No reason provided";
-
-		client.set_audit_reason(reason);
-		client.guild_member_kick(guild_id, user_id);
-
-		description = fmt::format("User `{}` has been kicked!", user.format_username());
-		send_success(description, event);
-	}
-	else
-	{
-		if (is_higher)
-			send_error(description, event);
+		const auto is_error = error_check(client, event, user_id, dpp::p_kick_members, "kick");
+		const auto client_roles = dpp::find_guild_member(guild_id, client.me.id).roles;
+		const auto user_roles = dpp::find_guild_member(guild_id, user_id).roles;
+	
+		uint8_t client_pos;
+		uint8_t	user_pos;
+	
+		if (!client_roles.empty() && !user_roles.empty() && (guild_member != guild->members.end()))
+		{
+			const auto client_pos = dpp::find_role(client_roles.at(0))->position;
+			const auto user_pos = dpp::find_role(user_roles.at(0))->position;
+	
+			if (client_pos < user_pos) {
+				description = fmt::format("Cannot kick `{}` because my role is lower", user.username);
+				is_higher = true;
+			}
+		}
+	
+		if (!std::get<bool>(is_error) && !is_higher)
+		{
+			const auto reason_param = event.get_parameter("reason");
+			const auto reason = std::holds_alternative<std::string>(reason_param)
+				? std::get<std::string>(reason_param)
+				: "No reason provided";
+	
+			client.set_audit_reason(reason);
+			client.guild_member_kick(guild_id, user_id);
+	
+			description = fmt::format("User `{}` has been kicked!", user.format_username());
+			send_success(description, event);
+		}
 		else
-			send_error(std::get<std::string>(is_error), event);
+		{
+			if (is_higher)
+				send_error(description, event);
+			else
+				send_error(std::get<std::string>(is_error), event);
+		}
 	}
 }
--- src/commands/moderation/nickname.cpp
+++ src/commands/moderation/nickname.cpp
@@ -10,17 +10,12 @@ void nickname(dpp::cluster& client, const dpp::slashcommand_t& event)
         ? std::get<std::string>(event.get_parameter("setnick"))
         : "";
     
+    const auto user = client.user_get_sync(user_id);
     const auto guild = dpp::find_guild(event.command.guild_id);
-    const auto get_user = client.user_get_sync(user_id);
-
-    dpp::user user = client.user_get_sync(user_id);
-    auto target_user = dpp::find_guild_member(event.command.guild_id, user_id);
-
-    const auto is_admin_user = guild->base_permissions(target_user).has(dpp::p_administrator);
     const auto guild_member = guild->members.find(user_id);
 
-    auto description = fmt::format("Nickname changed from `{}` is: `{}`", get_user.username, nickname);
     bool is_error = false;
+    auto description = fmt::format("Nickname changed from `{}` is: `{}`", user.username, nickname);
 
     if (guild_member == guild->members.end())
     {
@@ -30,30 +25,33 @@ void nickname(dpp::cluster& client, const dpp::slashcommand_t& event)
         is_error = true;
     }
 
-    if (user_id == guild->owner_id)
+    if (!is_error)
     {
-        description = fmt::format("Failed to change nickname from `{}`", get_user.username);
-        send_error(description, event);
+        auto target_user = dpp::find_guild_member(event.command.guild_id, user_id);
+        const auto is_admin_user = guild->base_permissions(target_user).has(dpp::p_administrator);
 
-        is_error = true;
-    }
-
-    if (!is_error && !is_admin_user)
-    {
-        if (nickname == "")
-            description = fmt::format("Nickname has reset from {}", get_user.username);
-
-        if (user_id == client.me.id)
+        if (user_id == guild->owner_id)
         {
-            client.guild_set_nickname(event.command.guild_id, nickname);
-            send_success(description, event);
+            description = fmt::format("Failed to change nickname from `{}`", user.username);
+            send_error(description, event);
         }
         else
         {
-            target_user.set_nickname(nickname);
-            client.guild_edit_member(target_user);
+            if (nickname == "")
+                description = fmt::format("Nickname has reset from {}", user.username);
     
-            send_success(description, event);
+            if (user_id == client.me.id)
+            {
+                client.guild_set_nickname(event.command.guild_id, nickname);
+                send_success(description, event);
+            }
+            else
+            {
+                target_user.set_nickname(nickname);
+                client.guild_edit_member(target_user);
+        
+                send_success(description, event);
+            }
         }
     }
 }
