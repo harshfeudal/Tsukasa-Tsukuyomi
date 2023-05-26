e92affe27175331b68d80cfd24a90ccdb694ba98
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-05-27 00:06:41 +0700

    [Add]: Lua configuration file

---
+++ .github/workflows/premake5.lua
@@ -0,0 +1,35 @@
+workspace "Tsukasa Tsukuyomi"
+	architecture "x64"
+	configurations { "Release" }
+
+project "Tsukasa Tsukuyomi"
+	kind          "ConsoleApp"
+	language      "C++"
+	cppdialect    "C++17"
+	staticruntime "off"
+
+	outputdir = "%{cfg.buildcfg}"
+
+	targetdir ("%{wks.location}/x64/%{cfg.buildcfg}")
+	objdir    ("%{wks.location}/x64/%{cfg.buildcfg}")
+
+	includedirs{ "include", "handler" }
+	libdirs    { "lib" }
+	links      { "dpp.lib", "opencv_world470d.lib" }
+	files      { 
+		"src/**.cpp", 
+		"src/cmd_lists/cmd_common/**.cpp",
+        "src/cmd_lists/cmd_moderation/**.cpp",
+        "src/cmd_engine/**.cpp",
+		"handler/**.h",
+        "handler/builder/**.h",
+		"Tsukasa Tsukuyomi.rc",
+        "resource.h"
+	}
+
+	filter { "system:windows", "toolset:msc" }
+		systemversion "latest"
+
+	filter "configurations:Release"
+		runtime  "Release"
+		optimize "on"
