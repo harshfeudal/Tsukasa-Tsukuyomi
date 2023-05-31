1b012c33464c3f227732f220e0cf95d1469351dc
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-06-01 00:22:29 +0700

    [Add]: Thinking about if I continue to add
    - I think the train data will be not included in this Discord bot, and will be soon removed

--- src/ocr_engine/training_model.cpp
+++ src/ocr_engine/training_model.cpp
@@ -1,1 +1,14 @@
 #include <ocr_engine.h>
+
+#include <fstream>
+
+bool is_file_exist(std::string filename)
+{
+    std::ifstream file(filename);
+    return !file ? false : true;
+}
+
+void file_load(cv::Mat& classifications, cv::Mat& training_img)
+{
+    
+}
