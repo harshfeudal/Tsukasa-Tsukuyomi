b02c0b00b127818e3ee0fae55ceb69611bb616d6
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-06-05 17:23:26 +0700

    [Building]: Making purge engine

--- src/cmd_engine/purge_engine.cpp
+++ src/cmd_engine/purge_engine.cpp
@@ -2,5 +2,16 @@
 
 void purge_engine(dpp::cluster& client, const dpp::slashcommand_t& event, dpp::snowflake channel_id, int64_t amount)
 {
-    
+    client.messages_get(channel_id, 0, 0, 0, amount, [&](const dpp::confirmation_callback_t& callback) {
+        if (!callback.is_error())
+        {
+            std::vector<dpp::snowflake> message_ids;
+            const auto message_delete_map = std::get<dpp::message_map>(callback.value);
+
+            for (const auto& message_deletion : message_delete_map)
+                message_ids.emplace_back(message_deletion.first);
+
+            client.message_delete_bulk(message_ids, channel_id);
+        }
+    });
 }
