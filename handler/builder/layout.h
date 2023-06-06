a275954150668553d8ebe05110c168631fbc99c6
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-06-07 00:30:19 +0700

    [Update]: No more error!
    - Bug finally fixed!

--- handler/builder/layout.h
+++ handler/builder/layout.h
@@ -66,7 +66,7 @@ inline std::map<std::string, Command> commands
             "Delete message(s)", purge,
             {
                 dpp::command_option(dpp::co_integer, "amount", "Amount of message(s) to delete", true)
-					.set_min_value(static_cast<int64_t>(2))
+					.set_min_value(static_cast<int64_t>(1))
 					.set_max_value(static_cast<int64_t>(1000)),
                 dpp::command_option(dpp::co_string, "reason", "The reason to purge, if any", false)
             }
--- src/cmd_lists/cmd_moderation/purge.cpp
+++ src/cmd_lists/cmd_moderation/purge.cpp
@@ -33,21 +33,51 @@ void purge(dpp::cluster& client, const dpp::slashcommand_t& event)
 
                     for (const auto& messages : message_map)
                     {
-                        if (!messages.second.pinned)
+                        if (messages.second.pinned) return;
+                        else
                             message_ids.emplace_back(messages.first);
                     }
 
-                    for (int i = 0; i < message_ids.size(); i++)
-                    {
-                        std::vector<dpp::snowflake> delete_id;
-                        delete_id.push_back(message_ids.at(i));
+                    bool is_single = false;
 
-                        if (delete_id.size() >= 100 || delete_id.at(i) == message_ids.at(message_ids.size() - 1))
+                    if (amount_messages <= 100)
+                        client.message_delete_bulk(message_ids, event.command.channel_id);
+                    else
+                    {
+                        if (amount_messages % 100 == 1)
                         {
-                            client.message_delete_bulk(delete_id, event.command.channel_id);
-                            delete_id.clear();
-                        } 
+                            client.message_delete(message_ids.at(0), event.command.channel_id);
+                            is_single = true;
+                        }
+
+                        const auto amount_division = amount_messages / 100;
+                        const auto amount_mod = amount_messages % 100;
+
+                        switch (amount_division)
+                        {
+                        case 0: break;
+                        default:
+                            auto single = 0;
+
+                            if (is_single)
+                                single = 1;
+
+                            for (int i = 0; i < amount_division; ++i)
+                            {
+                                std::vector<dpp::snowflake> delete_ids;
+                                for (int j = i * 100 + single; j <= i * 100 + single + 99; ++j)
+                                    delete_ids.push_back(message_ids.at(j));
+
+                                client.message_delete_bulk(delete_ids, event.command.channel_id);
+                                delete_ids.clear();
+                            }
+
+                            break;
+                        }
                     }
+
+                    const auto description = fmt::format("Deleted {} messages in <#{}>", amount_messages, event.command.channel_id);
+                    send_success(description, event);
                 }
                 else
                     send_error("Cannot delete too old messages, please try again", event);
