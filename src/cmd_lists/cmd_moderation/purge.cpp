2e400d57805b068fcec31edf4f350fddf65d1518
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-06-08 16:21:34 +0700

    [Bug]: Under-investigation

--- src/cmd_lists/cmd_moderation/purge.cpp
+++ src/cmd_lists/cmd_moderation/purge.cpp
@@ -6,81 +6,87 @@
 void purge(dpp::cluster& client, const dpp::slashcommand_t& event)
 {
     bool is_valid_to_purge = true;
-    const auto amount_messages = std::get<int64_t>(event.get_parameter("amount"));
+    bool is_purged = false;
+
+    auto amount_messages = std::get<int64_t>(event.get_parameter("amount"));
 
     const auto reason_param = event.get_parameter("reason");
     const auto reason = std::holds_alternative<std::string>(reason_param)
         ? std::get<std::string>(reason_param)
         : "No reason provided";
 
-    client.messages_get(event.command.channel_id, 0, 0, 0, amount_messages, [&](const dpp::confirmation_callback_t& callback)
+    client.messages_get(event.command.channel_id, 0, 0, 0, 2000, [&](const dpp::confirmation_callback_t& callback)
         {
             if (!callback.is_error())
             {
-                std::vector<time_t> message_collector;
-                const auto collector_map = std::get<dpp::message_map>(callback.value);
+                std::vector<dpp::snowflake> message_ids;
+                const auto message_map = std::get<dpp::message_map>(callback.value);
 
-                for (const auto& messages : collector_map)
-                    message_collector.emplace_back(messages.second.sent);
+                for (const auto& messages : message_map)
+                {
+                    if (messages.second.sent > time(0) + 1209600)
+                    {
+                        is_valid_to_purge = false;
+                        break;
+                    }
+                    else
+                    {
+                        if (!messages.second.pinned)
+                            message_ids.emplace_back(messages.first);
+                        else
+                            continue;
+                    }
+                }
 
-                if (message_collector.at(message_collector.size() - 1) < 1209601)
-                    is_valid_to_purge = false;
+                std::vector<dpp::snowflake> delete_ids;
+                for (int i = 0; i < amount_messages; ++i)
+                    delete_ids.push_back(message_ids.at(i));
 
                 if (is_valid_to_purge)
                 {
-                    std::vector<dpp::snowflake> message_ids;
-                    const auto message_map = std::get<dpp::message_map>(callback.value);
-
-                    for (const auto& messages : message_map)
+                    if (delete_ids.size() == 1)
                     {
-                        if (messages.second.pinned) return;
-                        else
-                            message_ids.emplace_back(messages.first);
+                        client.message_delete(delete_ids.at(0), event.command.channel_id);
+                        is_purged = true;
                     }
-
-                    bool is_single = false;
-
-                    if (amount_messages <= 100)
-                        client.message_delete_bulk(message_ids, event.command.channel_id);
                     else
                     {
-                        if (amount_messages % 100 == 1)
-                        {
-                            client.message_delete(message_ids.at(0), event.command.channel_id);
-                            is_single = true;
-                        }
-
-                        const auto amount_division = amount_messages / 100;
-                        const auto amount_mod = amount_messages % 100;
+                        const auto amount_division = message_ids.size() / 99;
 
                         switch (amount_division)
                         {
                         case 0: break;
                         default:
-                            auto single = 0;
-
-                            if (is_single)
-                                single = 1;
-
-                            for (int i = 0; i < amount_division; ++i)
+                            for (int i = 1; i <= amount_division; ++i)
                             {
-                                std::vector<dpp::snowflake> delete_ids;
-                                for (int j = i * 100 + single; j <= i * 100 + single + 99; ++j)
-                                    delete_ids.push_back(message_ids.at(j));
+                                std::vector<dpp::snowflake> delete_limit;
+                                for (int j = 0; j < i + 99; ++j)
+                                    delete_limit.push_back(delete_ids.at(j));
 
-                                client.message_delete_bulk(delete_ids, event.command.channel_id);
-                                delete_ids.clear();
+                                client.message_delete_bulk(delete_limit, event.command.channel_id);
+                                delete_limit.clear();
                             }
-
-                            break;
                         }
-                    }
 
-                    const auto description = fmt::format("Deleted {} messages in <#{}>", amount_messages, event.command.channel_id);
-                    send_success(description, event);
+                        const auto amount_remain = amount_messages - amount_division * 99;
+                        std::vector<dpp::snowflake> remain_delete;
+
+                        for (int i = amount_messages - amount_remain; i < amount_messages; ++i)
+                            remain_delete.push_back(delete_ids.at(i));
+
+                        client.message_delete_bulk(remain_delete, event.command.channel_id);
+                        is_purged = true;
+                    }
                 }
-                else
-                    send_error("Cannot delete too old messages, please try again", event);
             }
         });
+
+    if (!is_valid_to_purge)
+        send_error("Cannot delete too old messages, please try again", event);
+
+    if (is_purged)
+    {
+        const auto description = fmt::format("Deleted {} messages in <#{}>", amount_messages, event.command.channel_id);
+        send_success(description, event);
+    }
 }
