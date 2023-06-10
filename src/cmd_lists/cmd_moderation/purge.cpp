a4e19df54e14fe4b1ec789feba9023b34d98835d
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-06-10 12:20:38 +0700

    [Bug]: Response not show

--- src/cmd_lists/cmd_moderation/purge.cpp
+++ src/cmd_lists/cmd_moderation/purge.cpp
@@ -5,88 +5,132 @@
 
 void purge(dpp::cluster& client, const dpp::slashcommand_t& event)
 {
-    bool is_valid_to_purge = true;
-    bool is_purged = false;
-
     auto amount_messages = std::get<int64_t>(event.get_parameter("amount"));
+    int purged_message = 0;
 
+    bool is_finished = false;
+    bool is_error = false;
+
+    const auto channel_id = event.command.channel_id;
     const auto reason_param = event.get_parameter("reason");
+
     const auto reason = std::holds_alternative<std::string>(reason_param)
         ? std::get<std::string>(reason_param)
         : "No reason provided";
 
-    client.messages_get(event.command.channel_id, 0, 0, 0, 2000, [&](const dpp::confirmation_callback_t& callback)
-        {
-            if (!callback.is_error())
+    if (amount_messages <= 100)
+    {
+        client.messages_get(channel_id, 0, 0, 0, amount_messages, [&](const dpp::confirmation_callback_t& callback)
             {
+                const auto mesage_map = std::get<dpp::message_map>(callback.value);
                 std::vector<dpp::snowflake> message_ids;
-                const auto message_map = std::get<dpp::message_map>(callback.value);
 
-                for (const auto& messages : message_map)
+                for (const auto& message : mesage_map)
                 {
-                    if (messages.second.sent > time(0) + 1209600)
+                    if (time(0) - message.second.sent > 1209600 || message.second.author.username.empty() || message.second.pinned)
+                        continue;
+
+                    message_ids.emplace_back(message.first);
+                }
+
+                switch (message_ids.size())
+                {
+                case 0: 
+                    is_error = true;
+                    break;
+                case 1:
+                    client.message_delete(message_ids.at(0), channel_id);
+                    purged_message += 1;
+                    break;
+                default:
+                    client.message_delete_bulk(message_ids, channel_id);
+                    purged_message += message_ids.size();
+
+                    break;
+                }
+            });
+
+        if (is_error)
+            send_error("Cannot purge message older than 14 days", event);
+        else
+        {
+            if (is_finished)
+            {
+                auto description = fmt::format("Found and purged {} messages", purged_message);
+                send_success(description, event);
+            }
+        }
+    }
+    else
+    {
+        const int amount_division = amount_messages / 99;
+        const int amount_remain = amount_messages % 99;
+
+        for (size_t i = 1; i <= amount_division; i++)
+        {
+            client.messages_get(channel_id, 0, 0, 0, i * 99, [&](const dpp::confirmation_callback_t& callback)
+                {
+                    const auto mesage_map = std::get<dpp::message_map>(callback.value);
+                    std::vector<dpp::snowflake> message_ids;
+
+                    for (const auto& message : mesage_map)
                     {
-                        is_valid_to_purge = false;
+                        if (time(0) - message.second.sent > 1209600 || message.second.author.username.empty() || message.second.pinned)
+                            continue;
+
+                        message_ids.emplace_back(message.first);
+                    }
+
+                    switch (message_ids.size())
+                    {
+                    case 0: break;
+                    case 1:
+                        client.message_delete(message_ids.at(0), channel_id);
+                        purged_message += 1;
+                        break;
+                    default:
+                        client.message_delete_bulk(message_ids, channel_id);
+                        purged_message += message_ids.size();
                         break;
                     }
-                    else
-                    {
-                        if (!messages.second.pinned)
-                            message_ids.emplace_back(messages.first);
-                        else
-                            continue;
-                    }
-                }
+                });
+        }
 
-                std::vector<dpp::snowflake> delete_ids;
-                for (int i = 0; i < amount_messages; ++i)
-                    delete_ids.push_back(message_ids.at(i));
+        client.messages_get(channel_id, 0, 0, 0, amount_remain, [&](const dpp::confirmation_callback_t& callback)
+            {
+                const auto mesage_map = std::get<dpp::message_map>(callback.value);
+                std::vector<dpp::snowflake> message_ids;
 
-                if (is_valid_to_purge)
+                for (const auto& message : mesage_map)
                 {
-                    if (delete_ids.size() == 1)
-                    {
-                        client.message_delete(delete_ids.at(0), event.command.channel_id);
-                        is_purged = true;
-                    }
-                    else
-                    {
-                        const auto amount_division = message_ids.size() / 99;
+                    if (time(0) - message.second.sent > 1209600 || message.second.author.username.empty() || message.second.pinned)
+                        continue;
 
-                        switch (amount_division)
-                        {
-                        case 0: break;
-                        default:
-                            for (int i = 1; i <= amount_division; ++i)
-                            {
-                                std::vector<dpp::snowflake> delete_limit;
-                                for (int j = 0; j < i + 99; ++j)
-                                    delete_limit.push_back(delete_ids.at(j));
-
-                                client.message_delete_bulk(delete_limit, event.command.channel_id);
-                                delete_limit.clear();
-                            }
-                        }
-
-                        const auto amount_remain = amount_messages - amount_division * 99;
-                        std::vector<dpp::snowflake> remain_delete;
-
-                        for (int i = amount_messages - amount_remain; i < amount_messages; ++i)
-                            remain_delete.push_back(delete_ids.at(i));
-
-                        client.message_delete_bulk(remain_delete, event.command.channel_id);
-                        is_purged = true;
-                    }
+                    message_ids.emplace_back(message.first);
                 }
-            }
-        });
 
-    if (!is_valid_to_purge)
-        send_error("Cannot delete too old messages, please try again", event);
+                switch (message_ids.size())
+                {
+                case 0: break;
+                case 1:
+                    client.message_delete(message_ids.at(0), channel_id);
+                    purged_message += 1;
 
-    if (is_purged)
-    {
-        const auto description = fmt::format("Deleted {} messages in <#{}>", amount_messages, event.command.channel_id);
-        send_success(description, event);
+                    is_finished = true;
+                    break;
+                default:
+                    client.message_delete_bulk(message_ids, channel_id);
+                    purged_message += message_ids.size();
+
+                    is_finished = true;
+                    break;
+                }
+            });
+
+        if (is_finished && !is_error)
+        {
+            auto description = fmt::format("Found and purged {} messages", purged_message);
+            send_success(description, event);
+        }
     }
 }
