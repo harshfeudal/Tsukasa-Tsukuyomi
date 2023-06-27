5e3802f7e234f1c9683e400759a21a95b500601e
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2023-06-27 22:02:37 +0700

    [Bug]: Investigating

--- src/cmd_lists/cmd_moderation/purge.cpp
+++ src/cmd_lists/cmd_moderation/purge.cpp
@@ -6,9 +6,12 @@
 void purge(dpp::cluster& client, const dpp::slashcommand_t& event)
 {
     const auto amount_messages = std::get<int64_t>(event.get_parameter("amount"));
-
     const auto channel_id = event.command.channel_id;
+
     const auto reason_param = event.get_parameter("reason");
+    const auto reason = std::holds_alternative<std::string>(reason_param)
+		? std::get<std::string>(reason_param)
+		: "No reason provided";
 
     int purge_count = 0;
 
@@ -22,25 +25,20 @@ void purge(dpp::cluster& client, const dpp::slashcommand_t& event)
 
         for (const auto& message : message_map)
         {
-            if (message.second.sent < time(0) + 1209600 || message.second.pinned || message.second.author.username.empty())
+            if (message.second.sent > time(0) + 1209600 || message.second.pinned || message.second.author.username.empty())
                 continue;
 
             delete_ids.emplace_back(message.first);
         }
 
-        switch (delete_ids.size())
-        {
-        case 0:
-            break;
+        if (delete_ids.size() == 1)
+            client.message_delete_sync(delete_ids.at(0), channel_id);
 
-        case 1:
-            client.message_delete(delete_ids.at(0), channel_id);
-            break;
+        else if (delete_ids.size() > 1)
+            client.message_delete_bulk_sync(delete_ids, channel_id);
 
-        default:
-            client.message_delete_bulk(delete_ids, channel_id);
-            break;
-        }
+        else 
+            return;
 
         purge_count += delete_ids.size();
     }
@@ -56,21 +54,18 @@ void purge(dpp::cluster& client, const dpp::slashcommand_t& event)
         delete_ids.emplace_back(message.first);
     }
 
-    switch (delete_ids.size())
-    {
-    case 0:
-        break;
+    if (delete_ids.size() == 1)
+        client.message_delete_sync(delete_ids.at(0), channel_id);
 
-    case 1:
-        client.message_delete(delete_ids.at(0), channel_id);
-        break;
+    else if (delete_ids.size() > 1)
+        client.message_delete_bulk_sync(delete_ids, channel_id);
 
-    default:
-        client.message_delete_bulk(delete_ids, channel_id);
-        break;
-    }
+    else 
+        return;
 
     purge_count += delete_ids.size();
+
+    client.set_audit_reason(reason);
 
     auto description = fmt::format("Found and purged {} message{}", purge_count, purge_count > 1 ? "s" : "");
     send_success(description, event);
